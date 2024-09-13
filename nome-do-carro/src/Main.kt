cb72d049ddf6fb6ea9aed5afdda9a85492e71fa3
Emily Santana <imemilysantana@gmail.com>
2024-09-13 11:29:21 -0300

    Add files via upload

---
+++ nome-do-carro/src/Main.kt
@@ -0,0 +1,10 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    // Criação da variável e atribuição do valor "Fusca"
+    val nome_do_carro = "Fusca"
+
+    // Exibe o valor da variável ao usuário
+    println("O nome do carro é: $nome_do_carro")
+
+  }
---
+++ nome-do-carro/src/class.kt
@@ -0,0 +1,8 @@
+
+fun main() {
+    // Criação da variável e atribuição do valor "Fusca"
+    val nome_do_carro = "Fusca"
+
+    // Exibe o valor da variável ao usuário
+    println("O nome do carro é: $nome_do_carro")
+}
